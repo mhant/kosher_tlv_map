6a74d14203fac193718b8980c51b2295e23a53df
spledgoldstein <39864228+spledgoldstein@users.noreply.github.com>
2018-06-08 00:45:38 +0300

    Add files via upload
    Added file of just the places that need corrections.

---
+++ concise_corrected_addresses_bool.js
@@ -0,0 +1,161 @@
+var addresses = [{
+'rest_name':'בייקרס',
+'rest_type':'מסעדות',
+'kosher_type':'חלבי',
+'kosher_exp':'29/09/2018',
+'rest_addr':'נפחא 3',
+'lat':'32.0696789',
+'lng':'34.7718633',
+'reason':'The road is just called נפחא not יצחק נפחא',
+'id':'3513'
+},{
+'rest_name':'האופה מבגדד',
+'rest_type':'קונדיטוריות',
+'kosher_type':'חלבי,פרוה,מהדרין',
+'kosher_exp':'29/09/2018',
+'rest_addr':'אצ"ל 24',
+'lat':'32.0523723',
+'lng':'34.7891597',
+'reason':'The road name is actually אצ"ל not האצ"ל',
+'id':'3106'
+},{
+'rest_name':'הילולים',
+'rest_type':'מסעדות',
+'kosher_type':'בשרי,מהדרין',
+'kosher_exp':'29/09/2018',
+'rest_addr':'הלוחמים 1, Holon',
+'lat':'32.036491',
+'lng':'34.7594088',
+'reason':'The address is officially in Holon, not Tel Aviv',
+'id':'3081'
+},{
+'rest_name':'לב השכונה',
+'rest_type':'ירקנים',
+'kosher_type':'פרוה',
+'kosher_exp':'29/09/2018',
+'rest_addr':'אצ"ל 87',
+'lat':'32.0482852',
+'lng':'34.7931646',
+'reason':'The road name is actually אצ"ל not האצ"ל',
+'id':'3856'
+},{
+'rest_name':'לחם בוכרי',
+'rest_type':'קונדיטוריות',
+'kosher_type':'בשרי',
+'kosher_exp':'29/09/2018',
+'rest_addr':'שזר 11',
+'lat':'32.0386409',
+'lng':'34.7664039',
+'reason':'The street name is just שזר not זלמן שזר. Even though the sign on the shop itself says זלמן שזר',
+'id':'3130'
+},{
+'rest_name':'מאפיית לחם הקודש',
+'rest_type':'קונדיטוריות',
+'kosher_type':'חלבי,פרוה,מהדרין',
+'kosher_exp':'29/09/2018',
+'rest_addr':'אצ"ל 41',
+'lat':'32.0518497',
+'lng':'34.789501',
+'reason':'The road name is actually אצ"ל not האצ"ל',
+'id':'3113'
+},{
+'rest_name':'מלון אלכסנדר - גרנדביי',
+'rest_type':'בתי מלון',
+'kosher_type':'חלבי',
+'kosher_exp':'29/09/2018',
+'rest_addr':'חבקוק הנביא 3',
+'lat':'32.0936835',
+'lng':'34.7707663',
+'reason':'Full street name is חבקוק הנביא',
+'id':'3022'
+},{
+'rest_name':'סופר גלאט משה לאטי ובניו',
+'rest_type':'איטליזים',
+'kosher_type':'בשרי,מהדרין',
+'kosher_exp':'29/09/2018',
+'rest_addr':'נתן הנביא 4',
+'lat':'32.0524347',
+'lng':'34.791482',
+'reason':'Address is not complete. Full name of the street is נתן הנביא 4',
+'id':'2952'
+},{
+'rest_name':'Waffle Bar',
+'rest_type':'מסעדות',
+'kosher_type':'בשרי',
+'kosher_exp':'29/09/2018',
+'rest_addr':'אבן גבירול 61',
+'lat':'32.0787143',
+'lng':'34.7811354',
+'reason':'The name on the Rabanut website is Sushi Plus, but the restaurant is actually the Waffle Bar. The Teudah does have the name Waffle Bar on it, we checked!',
+'id':'3751'
+},{
+'rest_name':'פינת הקבב',
+'rest_type':'מסעדות',
+'kosher_type':'בשרי',
+'kosher_exp':'29/09/2018',
+'rest_addr':'אצ"ל 65',
+'lat':'32.0503935',
+'lng':'34.7898249',
+'reason':'The road name is actually אצ"ל not האצ"ל',
+'id':'3787'
+},{
+'rest_name':'פלאפל א.א.',
+'rest_type':'מסעדות',
+'kosher_type':'פרוה,מהדרין',
+'kosher_exp':'29/09/2018',
+'rest_addr':'הלוחמים 11',
+'lat':'32.0364944',
+'lng':'34.7593957',
+'reason':'This street address is in Holon',
+'id':'3091' 
+},{
+'rest_name':'פלאפל שרעבי',
+'rest_type':'מסעדות',
+'kosher_type':'בשרי',
+'kosher_exp':'29/09/2018',
+'rest_addr':'Lehi Rd 91',
+'lat':'32.0471859',
+'lng':'34.7974669',
+'reason':'Address is Derech Lehi, not Derech Lod',
+'id':'3542'
+},{
+'rest_name':'ציון עקיבא',
+'rest_type':'איטליזים',
+'kosher_type':'בשרי',
+'kosher_exp':'29/09/2018',
+'rest_addr':'יום טוב 1',
+'lat':'32.0679014',
+'lng':'34.765096',
+'reason':'The listed address יו"ט 1 is abbreviated and not googleable. Real address to google is: יום טוב 1',
+'id':'2967'
+},{
+'rest_name':'קליית יוסי',
+'rest_type':'מסעדות',
+'kosher_type':'פרוה',
+'kosher_exp':'29/09/2018',
+'rest_addr':'מטלון 47',
+'lat':'32.0588371',
+'lng':'34.7711944',
+'reason':'Street is just called מטלון not משה מטלון.',
+'id':'3265'
+},{
+'rest_name':'קקאו משפטים',
+'rest_type':'מסעדות',
+'kosher_type':'חלבי,בשרי',
+'kosher_exp':'29/09/2018',
+'rest_addr':'בניין משפטים, אוניברסיטת תל אביב',
+'lat':'32.1116819',
+'lng':'34.8035492',
+'reason':'Address could not be found on Google',
+'id':'3716'
+},{
+'rest_name':'רוקט',
+'rest_type':'מסעדות',
+'kosher_type':'בשרי',
+'kosher_exp':'29/09/2018',
+'rest_addr':'מוזס נח 22',
+'lat':'32.0755506',
+'lng':'34.7949556',
+'reason':'The street is not googleable. The full street name is מוזס יהודה ונח 22, תל אביב יפו or מוזס נח 22',
+'id':'3573'
+},];
