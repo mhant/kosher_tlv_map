42a6791dd20c99bc28aaf585516c42e5fae4970f
spledgoldstein <39864228+spledgoldstein@users.noreply.github.com>
2018-06-20 22:48:43 +0300

    Update for a few more corrections

--- correction_addresses.py
+++ correction_addresses.py
@@ -10,10 +10,10 @@ correction_addresses = {
 'rest_name':'בייקרס',
 'rest_type':'מסעדות',
 'kosher_type':'חלבי',
-'kosher_exp':'29/09/2018',
+#'kosher_exp':'29/09/2018',
 'rest_addr':'נפחא 3',
-'lat':'32.0696789',
-'lng':'34.7718633',
+#'lat':'32.0696789',
+#'lng':'34.7718633',
 'mod_type':'correction',
 'reason':'The road is just called נפחא not יצחק נפחא',
 'id':'3513'
@@ -27,10 +27,10 @@ correction_addresses = {
 'rest_name':'האופה מבגדד',
 'rest_type':'קונדיטוריות',
 'kosher_type':'חלבי,פרוה,מהדרין',
-'kosher_exp':'29/09/2018',
+#'kosher_exp':'29/09/2018',
 'rest_addr':'אצ"ל 24',
-'lat':'32.0523723',
-'lng':'34.7891597',
+#'lat':'32.0523723',
+#'lng':'34.7891597',
 'mod_type':'correction',
 'reason':'The road name is actually אצ"ל not האצ"ל',
 'id':'3106'
@@ -48,10 +48,10 @@ correction_addresses = {
 'rest_name':'הילולים',
 'rest_type':'מסעדות',
 'kosher_type':'בשרי,מהדרין',
-'kosher_exp':'29/09/2018',
+#'kosher_exp':'29/09/2018',
 'rest_addr':'הלוחמים 1',
-'lat':'32.036491',
-'lng':'34.7594088',
+#'lat':'32.036491',
+#'lng':'34.7594088',
 'mod_type':'correction',
 'reason':'The address is officially in Holon, not Tel Aviv',
 'id':'3081'
@@ -70,10 +70,10 @@ correction_addresses = {
 'rest_name':'לב השכונה',
 'rest_type':'ירקנים',
 'kosher_type':'פרוה',
-'kosher_exp':'29/09/2018',
+#'kosher_exp':'29/09/2018',
 'rest_addr':'אצ"ל 87',
-'lat':'32.0482852',
-'lng':'34.7931646',
+#'lat':'32.0482852',
+#'lng':'34.7931646',
 'mod_type':'correction',
 'reason':'The road name is actually אצ"ל not האצ"ל',
 'id':'3856'
@@ -88,10 +88,10 @@ correction_addresses = {
 'rest_name':'לחם בוכרי',
 'rest_type':'קונדיטוריות',
 'kosher_type':'בשרי',
-'kosher_exp':'29/09/2018',
+#'kosher_exp':'29/09/2018',
 'rest_addr':'שזר 11',
-'lat':'32.0386409',
-'lng':'34.7664039',
+#'lat':'32.0386409',
+#'lng':'34.7664039',
 'mod_type':'correction',
 'reason':'The street name is just שזר not זלמן שזר. Even though the sign on the shop itself says זלמן שזר',
 'id':'3130'
@@ -106,10 +106,10 @@ correction_addresses = {
 'rest_name':'מאפיית לחם הקודש',
 'rest_type':'קונדיטוריות',
 'kosher_type':'חלבי,פרוה,מהדרין',
-'kosher_exp':'29/09/2018',
+#'kosher_exp':'29/09/2018',
 'rest_addr':'אצ"ל 41',
-'lat':'32.0518497',
-'lng':'34.789501',
+#'lat':'32.0518497',
+#'lng':'34.789501',
 'mod_type':'correction',
 'reason':'The road name is actually אצ"ל not האצ"ל',
 'id':'3113'
@@ -123,10 +123,10 @@ correction_addresses = {
 'rest_name':'מלון אלכסנדר - גרנדביי',
 'rest_type':'בתי מלון',
 'kosher_type':'חלבי',
-'kosher_exp':'29/09/2018',
+#'kosher_exp':'29/09/2018',
 'rest_addr':'חבקוק הנביא 3',
-'lat':'32.0936835',
-'lng':'34.7707663',
+#'lat':'32.0936835',
+#'lng':'34.7707663',
 'mod_type':'correction',
 'reason':'Full street name is חבקוק הנביא',
 'id':'3022'
@@ -143,10 +143,10 @@ correction_addresses = {
 'rest_name':'סופר גלאט משה לאטי ובניו',
 'rest_type':'איטליזים',
 'kosher_type':'בשרי,מהדרין',
-'kosher_exp':'29/09/2018',
+#'kosher_exp':'29/09/2018',
 'rest_addr':'נתן הנביא 4',
-'lat':'32.0524347',
-'lng':'34.791482',
+#'lat':'32.0524347',
+#'lng':'34.791482',
 'mod_type':'correction',
 'reason':'Address is not complete. Full name of the street is נתן הנביא 4',
 'id':'2952'
@@ -163,10 +163,10 @@ correction_addresses = {
 'rest_name':'Waffle Bar',
 'rest_type':'מסעדות',
 'kosher_type':'חלבי',
-'kosher_exp':'29/09/2018',
+#'kosher_exp':'29/09/2018',
 'rest_addr':'אבן גבירול 61',
-'lat':'32.0787143',
-'lng':'34.7811354',
+#'lat':'32.0787143',
+#'lng':'34.7811354',
 'mod_type':'correction',
 'reason':'The name on the Rabanut website is Sushi Plus, but the restaurant is actually the Waffle Bar. The Teudah does have the name Waffle Bar on it, we checked!',
 'id':'3751'
@@ -182,10 +182,10 @@ correction_addresses = {
 'rest_name':'פינת הקבב',
 'rest_type':'מסעדות',
 'kosher_type':'בשרי',
-'kosher_exp':'29/09/2018',
+#'kosher_exp':'29/09/2018',
 'rest_addr':'אצ"ל 65',
-'lat':'32.0503935',
-'lng':'34.7898249',
+#'lat':'32.0503935',
+#'lng':'34.7898249',
 'mod_type':'correction',
 'reason':'The road name is actually אצ"ל not האצ"ל',
 'id':'3787'
@@ -202,10 +202,10 @@ correction_addresses = {
 'rest_name':'פלאפל א.א.',
 'rest_type':'מסעדות',
 'kosher_type':'פרוה,מהדרין',
-'kosher_exp':'29/09/2018',
+#'kosher_exp':'29/09/2018',
 'rest_addr':'הלוחמים 1',
-'lat':'32.036491',
-'lng':'34.7594088',
+#'lat':'32.036491',
+#'lng':'34.7594088',
 'mod_type':'query',
 'reason':'Address is: הלוחמים 1, Holon ?',
 'id':'3091' 
@@ -221,10 +221,10 @@ correction_addresses = {
 'rest_name':'פלאפל שרעבי',
 'rest_type':'מסעדות',
 'kosher_type':'בשרי',
-'kosher_exp':'29/09/2018',
+#'kosher_exp':'29/09/2018',
 'rest_addr':'Lehi Rd 91',
-'lat':'32.0471859',
-'lng':'34.7974669',
+#'lat':'32.0471859',
+#'lng':'34.7974669',
 'mod_type':'correction',
 'reason':'Address is Derech Lehi, not Derech Lod',
 'id':'3542'
@@ -241,10 +241,10 @@ correction_addresses = {
 'rest_name':'ציון עקיבא',
 'rest_type':'איטליזים',
 'kosher_type':'בשרי',
-'kosher_exp':'29/09/2018',
+#'kosher_exp':'29/09/2018',
 'rest_addr':'יום טוב 1',
-'lat':'32.0679014',
-'lng':'34.765096',
+#'lat':'32.0679014',
+#'lng':'34.765096',
 'mod_type':'correction',
 'reason':'The listed address יו"ט 1 is abbreviated and not googleable. Real address to google is: יום טוב 1',
 'id':'2967'
@@ -260,10 +260,10 @@ correction_addresses = {
 'rest_name':'קליית יוסי',
 'rest_type':'מסעדות',
 'kosher_type':'פרוה',
-'kosher_exp':'29/09/2018',
+#'kosher_exp':'29/09/2018',
 'rest_addr':'מטלון 47',
-'lat':'32.0588371',
-'lng':'34.7711944',
+#'lat':'32.0588371',
+#'lng':'34.7711944',
 'mod_type':'correction',
 'reason':'Street is just called מטלון not משה מטלון.',
 'id':'3265'
@@ -280,10 +280,10 @@ correction_addresses = {
 'rest_name':'קקאו משפטים',
 'rest_type':'מסעדות',
 'kosher_type':'חלבי,בשרי',
-'kosher_exp':'29/09/2018',
+#'kosher_exp':'29/09/2018',
 'rest_addr':'בניין משפטים, אוניברסיטת תל אביב',
-'lat':'32.1116819',
-'lng':'34.8035492',
+#'lat':'32.1116819',
+#'lng':'34.8035492',
 'mod_type':'correction',
 'reason':'Address could not be found on Google',
 'id':'3716'
@@ -299,10 +299,10 @@ correction_addresses = {
 'rest_name':'רוקט',
 'rest_type':'מסעדות',
 'kosher_type':'בשרי',
-'kosher_exp':'29/09/2018',
+#'kosher_exp':'29/09/2018',
 'rest_addr':'מוזס נח 22',
-'lat':'32.0755506',
-'lng':'34.7949556',
+#'lat':'32.0755506',
+#'lng':'34.7949556',
 'mod_type':'correction',
 'reason':'The street is not googleable. The full street name is מוזס יהודה ונח 22, תל אביב יפו or מוזס נח 22',
 'id':'3573'
@@ -316,10 +316,10 @@ correction_addresses = {
 'rest_name':'איטליט',
 'rest_type':'מסעדות',
 'kosher_type':'חלבי',
-'kosher_exp':'29/09/2018',
+#'kosher_exp':'29/09/2018',
 'rest_addr':'בן יהודה 69',
-'lat':'32.0803161',
-'lng':'34.7699044',
+#'lat':'32.0803161',
+#'lng':'34.7699044',
 'mod_type':'correction',
 'reason':'The restaurant name is איטליט  not איטלית ',
 'id':'3474'
@@ -335,13 +335,123 @@ correction_addresses = {
 'rest_name':'קייטרינג התאנה',
 'rest_type':'מסעדות',
 'kosher_type':'חלבי,בשרי',
-'kosher_exp':'29/09/2018',
+#'kosher_exp':'29/09/2018',
 'rest_addr':'מרמורק 28',
-'lat':'32.0711529',
-'lng':'34.78148',
+#'lat':'32.0711529',
+#'lng':'34.78148',
 'mod_type':'hide',
 'reason':'This is not a restaurant open to the public, it is a catering company',
 'id':'3713'
+},{
+
+'orig_rest_name':'שטראוס קפה בי.וי - קפה עלית',
+'orig_rest_type':'מסעדות',
+'orig_kosher_type':'חלבי',
+#'kosher_exp':'29/09/2018',
+'orig_rest_addr':'שד רוקח',
+
+
+'rest_name':'שטראוס קפה בי.וי - קפה עלית',
+'rest_type':'מסעדות',
+'kosher_type':'חלבי',
+'rest_addr':'95 שד רוקח',
+#'lat':'32.1007782',
+#'lng':'34.7968763'
+'mod_type':'correction',
+'reason':'Needs number on street',
+'id':'3348'
+
+},{
+
+'orig_rest_name':'שטראוס קפה בי.וי - קפה עלית',
+'orig_rest_type':'מסעדות',
+'orig_kosher_type':'חלבי',
+'orig_rest_addr':'ארלוזרוב',
+
+
+
+
+
+'rest_name':'שטראוס קפה בי.וי - קפה עלית',
+'rest_type':'מסעדות',
+'kosher_type':'חלבי',
+'rest_addr':'ארלוזרוב',
+'mod_type':'hide',
+'reason':'Ambiguous address. Need confirmation from Rabanut what and where this is.',
+'id':'3350 or 3351'
+
+
+
+},{
+'orig_rest_name':'שטראוס קפה בי.וי - קפה עלית',
+'orig_rest_type':'מסעדות',
+'orig_kosher_type':'חלבי',
+'orig_rest_addr':'ארלוזרוב',
+
+
+
+
+'rest_name':'שטראוס קפה בי.וי - קפה עלית',
+'rest_type':'מסעדות',
+'kosher_type':'חלבי',
+'rest_addr':'ארלוזרוב',
+'mod_type':'hide',
+'reason':'Ambiguous address. Need confirmation from Rabanut what and where this is.',
+'id':'3350 or 3351'
+},{
+
+'orig_rest_name':'בייבי עוף - שמואלי',
+'orig_rest_type':'איטליזים',
+'orig_kosher_type':'בשרי',
+'orig_rest_addr':'יו"ט 17',
+
+'rest_name':'בייבי עוף - שמואלי',
+'rest_type':'איטליזים',
+'kosher_type':'בשרי',
+'rest_addr':'יום טוב 17',
+
+'mod_type':'correction',
+'reason':'Need full street name, not abbreviation.',
+'id':'2977'
+
+
+},{
+'orig_rest_name':'קצב בני מבורך',
+'orig_rest_type':'איטליזים',
+'orig_kosher_type':'בשרי',
+'orig_rest_addr':'יו"ט 3',
+
+'rest_name':'קצב בני מבורך',
+'rest_type':'איטליזים',
+'kosher_type':'בשרי',
+'rest_addr':'יום טוב 3',
+
+
+'mod_type':'correction',
+'reason':'Need full street name, not abbreviation.',
+'id':'2978'
+
+},{
+
+
+'orig_rest_name':'בורגרים',
+'orig_rest_type':'מסעדות',
+'orig_kosher_type':'בשרי',
+'orig_rest_addr':'מתחם האוניברסיטה',
+
+'rest_name':'בורגרים',
+'rest_type':'מסעדות',
+'kosher_type':'בשרי',
+'rest_addr':'גורג וייז 20',
+
+'mod_type':'correction',
+'reason':'Real address available.',
+'id':'3603'
+
+
+#},{
+
+
 };
 
 
