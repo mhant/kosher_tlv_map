cd03d733860d176230a77b5f7366da1c5d4868b2
mhant <michael@androiddev101.com>
2018-06-27 09:25:59 +0300

    Adding ID scrape
    scraping ID from origional rabanut scrape and adding ID and rabanut URL to JSON

--- parser.py
+++ parser.py
@@ -2,14 +2,21 @@
 # -*- coding: utf-8 -*-
 
 import json
+import sys
 import requests
-from HTMLParser import HTMLParser
+from html.parser import HTMLParser
 import codecs
 import urllib
-import sys
+import urllib.parse
+from urllib.parse import quote   #now needed in python 3
+from urllib.parse import urlparse
+from urllib.parse import parse_qs
+import re
 import datetime
 
-from correction_addresses import * 
+from correction_addresses import *
+
+
 
 class KosherParser(HTMLParser):
 	in_td = False
@@ -31,6 +38,14 @@ class KosherParser(HTMLParser):
 			for name, value in attrs:
 				if name == 'class' and 't-b' in value:
 					self.in_table = True
+		elif tag == 'a' and self.in_td and self.in_table and self.in_tr:
+			for name, value in attrs:
+				if name == 'href':
+					parsed_url = urlparse(value)
+					item_id = parse_qs(parsed_url.query)['id'][0]
+					# need to parse out the int id because we get junk in there
+					item_id_int = re.search(r'\d+', item_id).group()
+					self.temp_row.append(item_id_int)
 
 	def handle_data(self, data):
 		if self.in_td:
@@ -55,12 +70,10 @@ class KosherParser(HTMLParser):
 			self.in_table = False
 
 if len(sys.argv) < 2:
-	print "Need google map key!"
+	print("Need google map key!")
 	sys.exit(22);
-	
-start_time = datetime.datetime.now();
 
-	
+start_time = datetime.datetime.now();
 goe_code_url_post = '&key=' + sys.argv[1]
 goe_code_url_pre = 'https://maps.googleapis.com/maps/api/geocode/json?address='
 kosher_url = "http://www.rabanut.co.il/Sapakim/show/comp/comps.aspx"
@@ -71,10 +84,8 @@ redux_text = r.text.replace('&','+').replace('\t','')
 parser.feed(redux_text)
 final_list = []
 tel_aviv_u = ' תל אביב יפו'
-tel_aviv = tel_aviv_u.decode('utf8')
+tel_aviv = tel_aviv_u # in python 3 all strings are decoded. no need for this :tel_aviv_u.decode('utf8')
 coords = {}
-
-
 typesToIgnore = ['בית ספר לבישול', 'בתי אבות','בתי חולים', 'מפעל', 'אולמות אירועים'];
 ignored = 0; #r_type was in typesToIgnore
 corrected = 0; #was in the corrections list
@@ -87,25 +98,25 @@ with codecs.open("corrected_addresses_bool.js", 'wb',  'utf-8') as f:
 	f.write("var addresses = [");
 	for rest in parser.table_data:
 		table_len = len(rest)
-		if (table_len == 6 or table_len == 5):
+		# make sure we have an ID and array of 6 or 7 (maybe only 6?)
+		# watch out for 0 size rows in the array
+		if (table_len >= 6 and table_len <= 7 and rest[0].isdigit()):
 			count = count + 1
 
-			name = rest[0].replace("'","")
-			r_type = rest[1].replace("'","")
-			kosher_type = rest[2].replace("'","")
-			place = 5
-			if table_len == 5:
-				place = 4
-			kosher_exp = rest[place].replace("'","")
-			location = rest[3] + tel_aviv
+			name = rest[1].replace("'","")
+			rest_id = rest[0].replace("'","")
+			r_type = rest[2].replace("'","")
+			kosher_type = rest[3].replace("'","")
+			kosher_exp = rest[table_len-1].replace("'","")
+			location = rest[4] + tel_aviv
 			location = location.replace("'","")
-			address = rest[3].replace("'","")
-			
+			address = rest[4].replace("'","")
+
 			#check if we should ignore this type
 			if ( r_type in typesToIgnore ):
 				ignored += 1;
 				continue;
-			
+
 			#now check if this is on our correction list
 			break1=False
 			for place in correction_addresses:
@@ -123,34 +134,57 @@ with codecs.open("corrected_addresses_bool.js", 'wb',  'utf-8') as f:
 						break1=True
 						break #out of the place loop
 			if( break1 ):
-				continue #next in the rest loop		
-			
-			
+				continue #next in the rest loop
+
+
 			f.write("{\n")
+			# id for rest
+			f.write("'rest_id':'")
+			f.write(rest_id)
+			f.write("',\n")
+			# gen the url for the rabanut item page
+			rab_url = 'http://www.rabanut.co.il/show_item.asp?itemId=185&levelId=60055&itemType=0&id=' + rest_id + '&xsltemplate=1'
+			f.write("'rest_rab_url':'")
+			f.write(rab_url)
+			f.write("',\n")
+
+			# add the kashrut type for later iterations
+			f.write("'kosher_list_type':'")
+			f.write("rabanut")
+			f.write("',\n")
+
 			# name of place
 			f.write("'rest_name':'")
-
 			f.write(name)
 			f.write("',\n")
+
 			# type of place
 			f.write("'rest_type':'")
-			
+
 			f.write(r_type)
 			f.write("',\n")
+
 			# type of koshrut (meat, milky, both)
 			f.write("'kosher_type':'")
 
 			f.write(kosher_type)
 			f.write("',\n")
+
+
 			# kashrut expiration
 			f.write("'kosher_exp':'")
 			f.write(kosher_exp)
 			f.write("',\n")
+
+
 			# street address
 			f.write("'rest_addr':'")
 			f.write(address)
 			f.write("',\n")
-			geo_url = goe_code_url_pre + urllib.quote(location.replace("'","").encode('utf-8')) + goe_code_url_post
+
+
+			#geo_url = goe_code_url_pre + urllib.parse.quote(location.replace("'","").encode('utf-8')) + goe_code_url_post
+			geo_url = goe_code_url_pre + urllib.parse.quote(location.encode('utf-8')) + goe_code_url_post
 			gr = requests.get(geo_url)
 			json = gr.json()
 			if ('results' in json and len(json['results']) > 0):
@@ -217,58 +251,58 @@ with codecs.open("concise_corrected_addresses_bool.js", 'wb',  'utf-8') as f:
 	f.write("var addresses = [");
 	for address in correction_addresses:
 			f.write("{\n")
-			
+
 			# name of place
 			f.write("'orig_rest_name':'")
-			
+
 			f.write(address['orig_rest_name'])
 			f.write("',\n")
-			
-			
+
+
 			# type of place
 			f.write("'orig_rest_type':'")
-			
+
 			f.write(address['orig_rest_type'])
 			f.write("',\n")
-			
-			
+
+
 			# type of koshrut (meat, milky, both)
 			f.write("'orig_kosher_type':'")
-			
+
 			f.write(address['orig_kosher_type'])
 			f.write("',\n")
-			
-						
-			
+
+
+
 			# street address
 			f.write("'orig_rest_addr':'")
 			f.write(address['orig_rest_addr'])
 			f.write("',\n")
 
-			
-			
-			
-			
+
+
+
+
 			# name of place
 			f.write("'rest_name':'")
-			
+
 			f.write(address['rest_name'])
 			f.write("',\n")
-			
-			
+
+
 			# type of place
 			f.write("'rest_type':'")
-			
+
 			f.write(address['rest_type'])
 			f.write("',\n")
-			
-			
+
+
 			# type of koshrut (meat, milky, both)
 			f.write("'kosher_type':'")
-			
+
 			f.write(address['kosher_type'])
 			f.write("',\n")
-			
+
 
 			# street address
 			f.write("'rest_addr':'")
@@ -279,17 +313,16 @@ with codecs.open("concise_corrected_addresses_bool.js", 'wb',  'utf-8') as f:
 			f.write("'mod_type':'")
 			f.write(address['mod_type'])
 			f.write("',\n")
-		
+
 			# street address
 			f.write("'reason':'")
 			f.write(address['reason'])
 			f.write("',\n")
-		
+
 			# street address
 			f.write("'id':'")
 			f.write(address['id'])
 			f.write("'\n},")
-		
+
 
 	f.write("];\n");
-	
